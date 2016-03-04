5696638c9911b67d34608629284119f5d2267a4e
Сергей <chepinoga-sv@yandex.ru>
2016-03-04 16:44:18 +0300

    work

--- app/src/main/java/com/example/user/fivesecondsrule/MainActivity.java
+++ app/src/main/java/com/example/user/fivesecondsrule/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import java.lang.reflect.Array;
 import java.util.ArrayList;
@@ -28,7 +29,10 @@ public class MainActivity extends AppCompatActivity {
         nextButton.setOnClickListener(new Button.OnClickListener() {
             public void onClick(View v) // клик на кнопку
             {
-                getQuestion(questions);
+                final TextView tTemper = (TextView) findViewById(R.id.textView);
+                String ques = "";
+                ques = getQuestion(questions);
+                tTemper.setText(ques); // отображение температуры
             }
         });
 
@@ -41,15 +45,23 @@ public class MainActivity extends AppCompatActivity {
 
     public ArrayList<String> formList(){
         ArrayList<String> list = new ArrayList<>();
-        list.add("Name three sphere things");
-        list.add("Name three clother on you");
-        list.add("Name three friends");
-        list.add("Remember three Oscar masters");
+        list.add("Назовите три круглых предмета");
+        list.add("Назовите три вещи, которые наденете на свою свадьбу");
+        list.add("Назовите три мультфильма Дисней");
+        list.add("Назовите трех обладателей Оскар");
+        list.add("Назовите три фильма жанра хоррор");
+        list.add("Назовите три самых смешных сериала");
         return list;
     }
 
     public String getQuestion(ArrayList<String> list){
-        int i = (int) (Math.random()*list.size());
+        int i = 0;
+        try {
+            i = (int) (Math.random()*list.size());
+             }
+        catch (Exception e) {
+            System.out.println("Wow, i = 1!");
+        }
         return list.get(i);
     }
 
